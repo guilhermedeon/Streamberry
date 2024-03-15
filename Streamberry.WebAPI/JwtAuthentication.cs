d4c16f0671235dfa2ff5eadb3f442ce299ad13fd
Guilherme Deon de Faria <guibyn@gmail.com>
2024-03-14 21:15:02 -0300

    Remoção Titulo incorreto, proveniente do snippet usado para adicionar funcionalidade de JWT no swagger

--- Streamberry.WebAPI/JwtAuthentication.cs
+++ Streamberry.WebAPI/JwtAuthentication.cs
@@ -24,7 +24,6 @@ namespace Streamberry.WebAPI
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "apiagenda", Version = "v1" });
 
                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
                 {
